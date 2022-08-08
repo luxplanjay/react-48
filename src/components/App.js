24d74e1f2cfb40973d3a2ef18fb2244e2cf14648
Alexander Repeta <alexander.repeta@gmail.com>
2022-08-08 20:17:16 +0300

    prepare examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,8 @@
-import { useMedia } from 'react-use';
 import { GlobalStyle } from './GlobalStyle';
-import { Recipe } from './Recipe/Recipe';
-import recipes from '../recipes.json';
-import { Box } from './Box';
 
 export const App = () => {
-  const isDesktop = useMedia('(min-width: 768px)');
-
   return (
     <>
-      <Box
-        as="header"
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <h1>Logo</h1>
-        {isDesktop ? <h1>Desktop nav</h1> : <h1>Mobile nav</h1>}
-      </Box>
-      {recipes.map((recipe, idx) => (
-        <Recipe key={idx} recipe={recipe} />
-      ))}
       <GlobalStyle />
     </>
   );
--- src/components/RecipeInfo/RecipeInfo.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const InfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`;
