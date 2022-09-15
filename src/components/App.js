5fb47081440339a68ff1e520f2b534fb99805c4c
Alexander Repeta <alexander.repeta@gmail.com>
2022-09-15 20:27:20 +0300

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
 import { GlobalStyle } from './GlobalStyle';
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-const HomePage = lazy(() => import('./HomePage.js'));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
       <GlobalStyle />
     </>
   );
--- src/components/HomePage.js
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <h1>Homepage</h1>;
-};
-
-export default HomePage;
